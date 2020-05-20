7be484bec920224030dcdcdaf8fb9560e6127353
Hualintang <59079687+Hualintang@users.noreply.github.com>
2020-05-20 14:50:22 +0800

    Update python分辨率.py

--- 005/python分辨率.py
+++ 005/python分辨率.py
@@ -4,4 +4,25 @@ from PIL import Image
 iPhone5_WIDTH = 1136
 iPhone5_HEIGHT = 640
 
+ext = ['jpg', 'jpeg', 'png']
+files = os.listdir(path)
+path = "D:/python测试/"
+
+def process_image(filename, mwidth=640, mheight=1136):
+    image = Image.open(filename)
+    w, h = image.size
+    if w <= mwidth and h <= mheight:
+        print(filename, 'is OK.')
+        return
+    if 1.0*w/mwidth > 1.0*h/mheight:
+        scale = 1.0*w/mwidth
+        new_im = image.resize((int(w/scale), int(h/scale)), Image.ANTIALIAS)
+    else:
+        scale = 1.0*h/mheight
+        new_im = image.resize((int(w/scale), int(h/scale)), Image.ANTIALIAS)
+    new_im.save('new-'+filename)
+    new_im.close()
     
+for file in files:
+if file.split('.')[-1] in ext:
+    process_image(file)
