9714b486cb37dd1a1a925c5d23d2881d5f9ba82e
Hualintang <59079687+Hualintang@users.noreply.github.com>
2020-05-22 08:27:08 +0800

    Update count_words.py

--- 006/count_words.py
+++ 006/count_words.py
@@ -1,32 +1,33 @@
 import os
 import re
-import glob
-from collections import OrderedDict
 os.chdir(r'D:/python测试/')
 
-def get_num(key_word, filename):
-    '''获得词汇出现次数'''
-    f = open(filename, 'r', encoding='utf-8').read()
-    re_zhengze = re.compile(r'[\s\,\;\.\n]{1}'+key_word+r'[\s\,\;\.\n]{1}')
-    numbers = re_zhengze.findall(f)
-    return len(numbers)
+def walk_dir(path):
+    file_path = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if f.lower().endswith('txt'):#endswith() 方法用于判断字符串是否以指定后缀结尾
+                file_path.append(os.path.join(root,f))
+               # file_path.append(join(root, f))
+    return file_path
 
-def article_analysis(dirs):
-    article = glob.glob(r'*.txt')
-    dictdata = OrderedDict()
-    for m in article:
-        doc = open(m, 'r', encoding='utf-8').read()
-        doc = re.findall(r'[\w\-\_\.\']+', doc)                    #获得单词list
-        doc = list(map(lambda x: x.strip('.'), doc))            #去除句号
-        for n in doc:
-            dictdata[n] = get_num(n, m)
-        a = OrderedDict(sorted(dictdata.items(), key=lambda x: x[1], reverse = True))   #dict排序
-        print('在 %s 中出现次数最多的单词是：' % m)
-        for c in a:
-            print(c+' : %s 次' % a[c])
-            break
-    return 0
-
-if __name__ == '__main__':
-    file = '.'
-    article_analysis(file)
+def find_key_word(filepath):
+    word_dic = {}
+    filename = os.path.basename(filepath)#返回filepath最后的文件名
+    with open(filepath,encoding='utf-8') as f:
+        text = f.read()
+        words_list = re.findall(r'[A-Za-z]+', text.lower())
+        for w in words_list:
+            if w in word_dic:
+                word_dic[w] += 1
+            else:
+                word_dic[w] = 1
+        sorted_word_list = sorted(word_dic.items(), key=lambda d: d[1])
+        '''
+        word_dic.items() 为待排序的对象；
+        key=lambda x: x[1] 为对前面的对象中的第二维数据（即value）的值进行排序
+        '''
+        print (u"在%s文件中，%s为关键词，共出现了%s次" %(filename, sorted_word_list[-1][0], sorted_word_list[-1][1]))
+if __name__ == "__main__":
+    for file_path in walk_dir(os.getcwd()):
+        find_key_word(file_path)
